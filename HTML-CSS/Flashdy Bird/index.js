77cc2c230d0506175c80382878322c6a44867cff
sonynanaVN <samsamsam0905@gmail.com>
2025-01-11 14:20:12 +0700

    init project

---
+++ HTML-CSS/Flashdy Bird/index.js
@@ -0,0 +1,55 @@
+var block = document.getElementById("block");
+var hole = document.getElementById("hole");
+var character = document.getElementById("character");
+var jumping = 0;
+var counter = 0;
+
+hole.addEventListener('animationiteration', () => {
+    var random = -((Math.random() * 300) + 150);
+    hole.style.top = random + "px";
+    counter++;
+});
+
+setInterval(function () {
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if (jumping == 0) {
+        // Tốc độ rơi
+        character.style.top = (characterTop + 2) + "px";
+    }
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    var cTop = -(500 - characterTop);
+    if ((characterTop > 480) || ((blockLeft < 20) && (blockLeft > -50) && ((cTop < holeTop) || (cTop > holeTop + 130)))) {
+        alert("Game Over. Score:" + (counter - 1));
+        character.style.top = 100 + "px";
+        counter = 0;
+    }
+}, 10);
+
+function jump() {
+    if (jumping == 0) {
+        jumping = 1;
+        let jumpCount = 0;
+        var jumpInterval = setInterval(function () {
+            var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+            // Tăng độ cao nhảy, ví dụ thay đổi từ 5px thành 8px
+            if ((characterTop > 6) && (jumpCount < 20)) {
+                character.style.top = (characterTop - 6) + "px"; // Độ cao nhảy cao hơn
+            }
+            if (jumpCount > 30) {
+                clearInterval(jumpInterval);
+                jumping = 0;
+                jumpCount = 0;
+            }
+            jumpCount++;
+        }, 10);
+    }
+}
+
+// Lắng nghe sự kiện nhấn phím Space để thực hiện nhảy
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Space') {  // Kiểm tra nếu phím nhấn là Space
+        jump();
+    }
+});
+
